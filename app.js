e157c43d3c083973b679b31908d9f69d4e655577
mohil <jamohile@gmail.com>
2017-05-15 17:18:51 -0400

    made portfolio dynamic, linked to mongo

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ var express = require('express')
 var app = express();
 var router = express.Router();
 
+MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://jamohile:hj47rr6r6qId1rzx@cluster0-shard-00-00-ncgje.mongodb.net:27017,cluster0-shard-00-01-ncgje.mongodb.net:27017,cluster0-shard-00-02-ncgje.mongodb.net:27017/JayMohile?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+
+
 // all environments
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3002);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,7 +25,20 @@ var jsonParser = bodyParser.json();
 var urlParser = bodyParser.urlencoded({extended: false});
 
 app.get('/', urlParser, function(req, res){
-	res.sendFile('../public/index.html');
+	console.log('hello');
+	MongoClient.connect(url, function(err, db){
+		var collection = db.collection('portfolio_software');
+		collection.find({}, function(err, cursor){
+			cursor.toArray().then(function(data) {
+				console.log(data);
+				res.render('../views/index', {
+			        portfolio: data
+			    });
+				//res.json(data[0]);
+		});
+	});
+});
+	//res.sendFile('../public/index.html');
 });
 http.createServer(app).listen(app.get('port'), function(){
   console.log('JayMohile is active on port ' + app.get('port'));
